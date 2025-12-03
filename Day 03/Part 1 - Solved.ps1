aa53837386dd5cdc2189ea4eb475a9ac29b79697
Cato Larsen <lostlogic@outlook.com>
2025-12-03 08:49:14 +0100

    Day 3

---
+++ Day 03/Part 1 - Solved.ps1
@@ -0,0 +1,45 @@
+﻿$puzzleInput = Get-Content -Path "Input.txt"
+
+function MaxIntIndex ([string]$IntString, [int]$StartPos = 0, [bool]$NotLast = $false)
+{
+    $maxVal = 0
+    $maxValIndex = 0
+
+    $maxEndPos = $IntString.Length
+
+    if($NotLast)
+    {
+        $maxEndPos--
+    }
+
+    for($i = $StartPos; $i -lt $maxEndPos; $i++)
+    {
+        if([int]$IntString[$i] -gt $maxVal)
+        {
+            $maxVal = [int]$IntString[$i]
+            $maxValIndex = $i
+        }
+
+        if($maxVal -eq 9)
+        {
+            break
+        }
+    }
+
+    return $maxValIndex
+}
+
+$maxJoltTotal = 0
+
+foreach($line in $puzzleInput)
+{
+    $battPos1 = MaxIntIndex -IntString $line -StartPos 0 -NotLast $true
+    $battPos2 = MaxIntIndex -IntString $line -StartPos ($battPos1 + 1) -NotLast $false
+
+    $joltage = [int]"$($line[$battPos1])$($line[$battPos2])"
+
+    Write-Host "Bank $line : Joltage: $joltage" -ForegroundColor Cyan
+    $maxJoltTotal += $joltage
+}
+
+Write-Host "Max Total Joltage: $($maxJoltTotal)" -ForegroundColor Green
---
+++ Day 03/Part 2 - Solved.ps1
@@ -0,0 +1,45 @@
+﻿$puzzleInput = Get-Content -Path "Input.txt"
+
+function MaxIntIndex ([string]$IntString, [int]$StartPos = 0, [int]$EndPos = 0)
+{
+    $maxVal = 0
+    $maxValIndex = 0
+
+    for($i = $StartPos; $i -lt $EndPos; $i++)
+    {
+        if([int]$IntString[$i] -gt $maxVal)
+        {
+            $maxVal = [int]$IntString[$i]
+            $maxValIndex = $i
+        }
+
+        if($maxVal -eq 9)
+        {
+            break
+        }
+    }
+
+    return $maxValIndex
+}
+
+[long]$maxJoltTotal = 0
+$batteryTotal = 12
+
+foreach($line in $puzzleInput)
+{
+    $startPos = 0
+    $battCells = ""
+
+    for($i = 0; $i -lt $batteryTotal; $i++)
+    {
+        $maxEndPos = $line.Length - ($batteryTotal - ($i+1))
+        $startPos = MaxIntIndex -IntString $line -StartPos $startPos -EndPos $maxEndPos
+        $battCells += $line[$startPos]
+        $startPos++
+    }
+
+    Write-Host "Cell: $line - Jolt: $battCells" -ForegroundColor Cyan
+    $maxJoltTotal += [long]$battCells
+}
+
+Write-Host "Max Total Joltage: $($maxJoltTotal)" -ForegroundColor Green
